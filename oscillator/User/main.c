cbbe3ca0c92c6a7c9b03b51a089cb8cf719ba8fc
Lars Boegild Thomsen <lth@netcompartner.com>
2022-07-20 20:18:23 +0800

    Hmmmm

--- oscillator/User/main.c
+++ oscillator/User/main.c
@@ -8,6 +8,7 @@
  *******************************************************************************/
 
 /*
+ *
  * Overall principle is that the DACs will be driven by a DMA buffer of 48
  * sample values updated every 1 ms resulting in a sample rate of 48000 Hz
  *
@@ -39,8 +40,9 @@ typedef struct {
 Osc_TypeDef osc[2]; // Two oscillators
 
 uint32_t dac_buffer[2 * BUFFER_SIZE]; // High 16 bit dac out 2, low 16 bit dac out 1
+uint32_t adc_buffer[2 * BUFFER_SIZE];
 
-uint32_t full_count = 0, half_count = 0;
+uint32_t full_count = 0, half_count = 0, adc_count = 0;
 
 static inline void update_dac_buffer (uint32_t *buffer_address) {
     for (uint8_t sample = 0; sample < BUFFER_SIZE; ++sample) {
@@ -75,6 +77,18 @@ __attribute__((interrupt("WCH-Interrupt-fast"))) void DMA2_Channel3_IRQHandler (
 
 }
 
+__attribute__((interrupt("WCH-Interrupt-fast"))) void DMA1_Channel1_IRQHandler () {
+
+    // To time the ISR throw debug out hi
+    GPIO_WriteBit (GPIOA, GPIO_Pin_6, Bit_SET);
+
+    ++adc_count;
+
+    // Finally toggle debug out low again
+    GPIO_WriteBit (GPIOA, GPIO_Pin_6, Bit_RESET);
+
+}
+
 void DMA_Interrupt_Init () {
     /*Configuration interrupt priority*/
     NVIC_InitTypeDef NVIC_InitStructure = { 0 };
@@ -82,6 +96,9 @@ void DMA_Interrupt_Init () {
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1; //Seeing priority
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0; //Response priority
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //Enable
+    NVIC_Init (&NVIC_InitStructure);
+
+    NVIC_InitStructure.NVIC_IRQChannel = DMA1_Channel1_IRQn;
     NVIC_Init (&NVIC_InitStructure);
 }
 
@@ -92,7 +109,7 @@ void DMA_Interrupt_Init () {
  *
  * @return  none
  */
-void Dual_Dac_Init (void) {
+void DACs_Init (void) {
     GPIO_InitTypeDef GPIO_InitStructure = { 0 };
     DAC_InitTypeDef DAC_InitType = { 0 };
 
@@ -136,7 +153,7 @@ void Dual_Dac_Init (void) {
  *
  * @return  none
  */
-void Dac_Dma_Init (void) {
+void DAC_DMA_Init (void) {
     DMA_InitTypeDef DMA_InitStructure = { 0 };
     RCC_AHBPeriphClockCmd (RCC_AHBPeriph_DMA2, ENABLE);
 
@@ -162,16 +179,16 @@ void Dac_Dma_Init (void) {
 
 }
 
-void ADC_DMA_Init() {
+void ADCs_Init () {
     GPIO_InitTypeDef GPIO_InitStructure = { 0 };
-    ADC_InitTypeDef ADC_InitType = {0};
+    ADC_InitTypeDef ADC_InitType = { 0 };
 
     // Make sure the APB busses are clocked
     RCC_APB2PeriphClockCmd (RCC_APB2Periph_GPIOA, ENABLE);
     RCC_APB2PeriphClockCmd (RCC_APB2Periph_GPIOB, ENABLE);
     RCC_APB2PeriphClockCmd (RCC_APB2Periph_GPIOC, ENABLE);
-    RCC_APB1PeriphClockCmd (RCC_APB2Periph_ADC1, ENABLE);
-    RCC_APB1PeriphClockCmd (RCC_APB2Periph_ADC2, ENABLE);
+    RCC_APB2PeriphClockCmd (RCC_APB2Periph_ADC1, ENABLE);
+    RCC_APB2PeriphClockCmd (RCC_APB2Periph_ADC2, ENABLE);
 
     // Configure PC4 and PC5 for analog input
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5;
@@ -191,10 +208,55 @@ void ADC_DMA_Init() {
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init (GPIOA, &GPIO_InitStructure);
 
-    ADC_InitType.ADC_Mode = ADC_Mode_RegSimult_AlterTrig;
+    ADC_InitType.ADC_Mode = ADC_Mode_Independent;
     ADC_InitType.ADC_ScanConvMode = ENABLE;
-    ADC_InitType.ADC_ContinuousConvMode = DISABLE;
-    ADC_InitType.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T8_TRGO;
+    ADC_InitType.ADC_ContinuousConvMode = ENABLE;
+    ADC_InitType.ADC_DataAlign = ADC_DataAlign_Right;
+    ADC_InitType.ADC_NbrOfChannel = 1;
+    ADC_InitType.ADC_OutputBuffer = ADC_OutputBuffer_Enable;
+    ADC_InitType.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T3_TRGO;
+
+    ADC_Init (ADC1, &ADC_InitType);
+
+//    ADC_InjectedSequencerLengthConfig (ADC1, 1);
+//    ADC_InjectedChannelConfig (ADC1, ADC_Channel_TempSensor, 1, ADC_SampleTime_239Cycles5);
+//
+//    ADC_ExternalTrigInjectedConvConfig (ADC1, ADC_ExternalTrigConv_T3_TRGO);
+//    ADC_ExternalTrigInjectedConvCmd (ADC1, ENABLE);
+
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 1, ADC_SampleTime_7Cycles5 );
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_TempSensor, 2, ADC_SampleTime_7Cycles5 );
+
+    ADC_DMACmd (ADC1, ENABLE);
+    ADC_Cmd (ADC1, ENABLE);
+
+//    ADC_BufferCmd(ADC1, DISABLE);   //disable buffer
+//    ADC_ResetCalibration(ADC1);
+//    while(ADC_GetResetCalibrationStatus(ADC1));
+//    ADC_StartCalibration(ADC1);
+//    while(ADC_GetCalibrationStatus(ADC1));
+//    Calibrattion_Val1 = Get_CalibrationValue(ADC1);
+
+}
+
+void ADC_DMA_Init () {
+    DMA_InitTypeDef DMA_InitStructure = { 0 };
+
+    RCC_AHBPeriphClockCmd ( RCC_AHBPeriph_DMA1, ENABLE);
+
+    DMA_DeInit (DMA1_Channel1);
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (u32) &ADC1->RDATAR;
+    DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t) &adc_buffer[0];
+    DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
+    DMA_InitStructure.DMA_BufferSize = 2 * BUFFER_SIZE;
+    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+    DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+    DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+    DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+    DMA_InitStructure.DMA_Priority = DMA_Priority_VeryHigh;
+    DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+    DMA_Init ( DMA1_Channel1, &DMA_InitStructure);
 
 }
 
@@ -207,6 +269,7 @@ void ADC_DMA_Init() {
  */
 void Timer_Init (void) {
     TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure = { 0 };
+
     RCC_APB2PeriphClockCmd (RCC_APB2Periph_TIM8, ENABLE);
 
     TIM_TimeBaseStructInit (&TIM_TimeBaseStructure);
@@ -218,6 +281,15 @@ void Timer_Init (void) {
 
     TIM_SelectOutputTrigger (TIM8, TIM_TRGOSource_Update);
     TIM_Cmd (TIM8, ENABLE);
+
+    RCC_APB1PeriphClockCmd (RCC_APB1Periph_TIM3, ENABLE);
+
+    TIM_TimeBaseStructInit (&TIM_TimeBaseStructure);
+    TIM_TimeBaseInit (TIM3, &TIM_TimeBaseStructure);
+
+    TIM_SelectOutputTrigger (TIM3, TIM_TRGOSource_Update);
+    TIM_Cmd (TIM3, ENABLE);
+
 }
 
 void set_freq (Osc_TypeDef *osc, float freq) {
@@ -243,53 +315,57 @@ int main (void) {
     printf ("SystemClk:%d\r\n", SystemCoreClock);
     printf ("Dual DAC Generation Test\r\n");
 
-    set_freq (&osc[0], 110);
-    set_amplitude (&osc[0], 1);
-    set_freq (&osc[1], 440);
-    set_amplitude (&osc[1], 0.4);
+    set_freq (&osc[0], 250);
+    set_amplitude (&osc[0], 0.95);
+    set_freq (&osc[1], 249.9);
+    set_amplitude (&osc[1], 0.45);
 
     DMA_Interrupt_Init ();
-    Dual_Dac_Init ();
-    Dac_Dma_Init ();
+
+    DACs_Init ();
+    DAC_DMA_Init ();
+
+    ADCs_Init ();
+    ADC_DMA_Init ();
+
     Timer_Init ();
 
     uint32_t now = 0, last_tick = 0, last_amp = 0, last_freq = 0;
 
-
-    float amp = 0.2, amp_change = 0.1;
+    float amp = 0, amp_change = 0.01;
     float start_freq = 110, end_freq = 220, freq_change = 10, freq = 110;
 
     while (1) {
 
         now = GetTick ();
 
-        if (now - last_amp >= 100) {
+        if (now - last_amp >= 20) {
 
             set_amplitude(&osc[1], amp);
 
             amp += amp_change;
 
-            if (amp < 0.2) amp_change = 0.1;
-            if (amp > 0.8) amp_change = -0.1;
+            if (amp < 0.1) amp_change = 0.01;
+            if (amp > 0.9) amp_change = -0.01;
 
             last_amp = now;
         }
 
-        if (now - last_freq >= 100) {
-
-            set_freq(&osc[0], freq);
-
-            freq += freq_change;
-
-            if (freq >= end_freq) freq_change = -10;
-            if (freq <= start_freq) freq_change = 10;
-
-            last_freq = now;
-        }
+//        if (now - last_freq >= 100) {
+//
+//            set_freq(&osc[0], freq);
+//
+//            freq += freq_change;
+//
+//            if (freq >= end_freq) freq_change = -10;
+//            if (freq <= start_freq) freq_change = 10;
+//
+//            last_freq = now;
+//        }
 
         if (now - last_tick >= 1000) {
 
-            printf ("Tick %lu: tim8 = %lu full = %lu half = %lu\n", now / 1000, TIM8->CNT, full_count, half_count);
+            printf ("Tick %lu: full = %lu half = %lu adc = %lu\n", now / 1000, full_count, half_count, adc_count);
 
             last_tick = now;
         }
